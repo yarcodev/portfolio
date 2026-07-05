4a6591be9bcb01f24f9ddcd01d1f5768e13ca6f8
yarcodev <yarzarminhtike9@gmail.com>
2026-07-05 14:13:52 +0700

    modified js of search-box

--- script.js
+++ script.js
@@ -2,11 +2,17 @@ const hamburger = document.querySelector('.hamburger');
 const nav = document.querySelector('nav');
 const footerCopy = document.querySelector('.footer-copy');
 const search = document.querySelector('.search-box');
-
+const searchInput = document.querySelector('.search-box input');
 
 
 search.addEventListener('click', () => {
   search.classList.toggle('open');
+  if (search.classList.contains('open')) {
+    searchInput.focus(); // auto focus keyboard when opened
+  }
+});
+searchInput.addEventListener('click', (e) => {
+  e.stopPropagation();
 });
 
 hamburger.addEventListener('click', () => {
